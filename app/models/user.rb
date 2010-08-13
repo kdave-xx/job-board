6bab073581624f46f2acbfe19d81a421a044a5ca
kalpesh dave <kdave@fourthmedia.co.uk>
2010-08-13 11:38:32 +0530

    validations in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,9 @@ class User < ActiveRecord::Base
   has_many :jobs
 
   validates_uniqueness_of :login, :email
-  validates_presence_of :login
-  validates_presence_of :email
-  validates_presence_of :password
-
+  validates_presence_of :login, :email, :password
+  validates_length_of :login, :within => 6..32
+  validates_length_of :password, :maximum => 32
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
 
   def is_admin?
