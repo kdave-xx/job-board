8ef137d16d9abe3b8fd9cdc546745c2ae3c90a64
kalpesh dave <kdave@fourthmedia.co.uk>
2010-08-14 16:44:51 +0530

    error messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,19 @@
 class User < ActiveRecord::Base
-  acts_as_authentic
+  acts_as_authentic do |c|
+    c.validate_login_field = false
+    c.validate_email_field = false
+  end
 
   has_many :jobs
 
-
+  validates_presence_of :login, :email
+  validates_uniqueness_of :login, :unless => Proc.new{|u| u.attributes['login'].blank?}
+  validates_uniqueness_of :email, :unless => Proc.new{|u| u.attributes['email'].blank?}
   validates_length_of :login, :within => 6..32, :unless => Proc.new{|u| u.attributes['login'].blank?}
-  validates_length_of :password, :maximum => 32, :unless => Proc.new{|u| u.attributes['password'].blank?}
+  validates_length_of :email, :within => 6..50, :unless => Proc.new{|u| u.attributes['email'].blank?}
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :unless => Proc.new{|u| u.attributes['email'].blank?}
   validates_format_of :login, :with => /[A-Za-z0-9]+/, :unless => Proc.new{|u| u.attributes['login'].blank?}
+ 
   
   def is_admin?
     return is_admin
