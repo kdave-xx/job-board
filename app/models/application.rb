18a1c6ece8350b1e1bc91d95f4e8936300538ec7
kalpesh dave <kdave@fourthmedia.co.uk>
2010-08-16 16:01:02 +0530

    upload

--- app/models/application.rb
+++ app/models/application.rb
@@ -6,7 +6,7 @@ class Application < ActiveRecord::Base
                   :url  => "/data/applications/:id/:basename.:extension",
                   :path => ":rails_root/public/data/applications/:id/:basename.:extension"
 
-  validates_attachment_presence :data
+#  validates_attachment_presence :data
   validates_attachment_size :data, :less_than => 5.megabytes
   validates_attachment_content_type :data, :content_type => ['application/pdf', 'text/plain']
 
