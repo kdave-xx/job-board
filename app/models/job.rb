6b21f21cfa90f6151a5fdacc5bf3bb8b072d7ce9
kdave <kdave@fourthmedia.co.uk>
2010-08-17 16:08:27 +0530

    validation error

--- app/models/job.rb
+++ app/models/job.rb
@@ -11,8 +11,8 @@ class Job < ActiveRecord::Base
   has_many :applicants, :through => "application"
 
   validates_presence_of :title, :city, :start_date, :end_date
-  validates_format_of :title, :with => /[A-Za-z0-9]+/, :unless => Proc.new{|u| u.attributes['title'].blank?}
-  validates_format_of :city, :with => /[A-Za-z0-9]+/, :unless => Proc.new{|u| u.attributes['city'].blank?}
+  validates_format_of :title, :with => /^[A-Za-z0-9]+$/, :unless => Proc.new{|u| u.attributes['title'].blank?}
+  validates_format_of :city, :with => /^[A-Za-z0-9]+$/, :unless => Proc.new{|u| u.attributes['city'].blank?}
   validates_format_of :start_date, :with => /[A-Za-z0-9]+/, :unless => Proc.new{|u| u.attributes['start_date'].blank?}
   validates_format_of :end_date, :with => /[A-Za-z0-9]+/, :unless => Proc.new{|u| u.attributes['end_date'].blank?}
 end
