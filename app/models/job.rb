f97d06b011c81c801fc68c43d8519275575c678a
kdave <kdave@fourthmedia.co.uk>
2010-08-19 12:12:20 +0530

    titel

--- app/models/job.rb
+++ app/models/job.rb
@@ -13,6 +13,8 @@ class Job < ActiveRecord::Base
   validates_presence_of :title, :city, :start_date, :end_date
   validates_format_of :title, :with => /^[A-Za-z0-9\s&.-]+$/, :unless => Proc.new{|u| u.attributes['title'].blank?}
   validates_format_of :city, :with => /^[A-Za-z0-9\s]+$/, :unless => Proc.new{|u| u.attributes['city'].blank?}
+  validates_length_of :title, :maximum => 50, :unless => Proc.new{|u| u.attributes['title'].blank?}
+  validates_length_of :city, :maximum => 50, :unless => Proc.new{|u| u.attributes['city'].blank?}
   validate :valid_date
   
   def valid_date
